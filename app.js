b327d4f9e85dbb56c29459cdd73d6c90fbd2929b
Mike Filicetti <filicetti.michael@gmail.com>
2018-10-25 09:44:44 -0700

    I updated my html & JS files for ease of reading.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var correctAnswers = 0;
-var incorrectAnswers = 0;
 console.log('userName should be undefined:', userName);
 var userName = prompt('Welcome to my site. What is your name?');
 alert('Thank you for visiting my site, ' + userName + '!');
@@ -9,6 +8,7 @@ console.log('userName', userName);
 
 alert('Please respond to the following questions with Y/YES or N/NO');
 
+//first question
 console.log('responseOne should be undefined:', responseOne);
 var responseOne = prompt('Hello, ' + userName + '! Do you think you know me?').toUpperCase();
 if (responseOne === 'Y' || responseOne === 'YES') {
@@ -16,18 +16,16 @@ if (responseOne === 'Y' || responseOne === 'YES') {
   correctAnswers++;
 } else {
   alert('Best of luck then!');
-  incorrectAnswers++;
 }
 console.log('responseOne', responseOne);
 
 console.log(correctAnswers);
-console.log(incorrectAnswers);
 
+//second question
 console.log('responseTwo should be undefined:', responseTwo);
 var responseTwo = prompt('Was I born in WA state?').toUpperCase();
 if (responseTwo === 'Y' || responseTwo === 'YES') {
   alert('Sorry! I was a California baby.');
-  incorrectAnswers++;
 } else {
   alert('You got it! I was a California baby.');
   correctAnswers++;
@@ -35,8 +33,8 @@ if (responseTwo === 'Y' || responseTwo === 'YES') {
 console.log('responseTwo', responseTwo);
 
 console.log(correctAnswers);
-console.log(incorrectAnswers);
 
+//third question
 console.log('responseThree should be undefined:', responseThree);
 var responseThree = prompt('Alright ' + userName + ', is pizza my favorite food?').toUpperCase();
 if (responseThree === 'Y' || responseThree === 'YES') {
@@ -44,13 +42,12 @@ if (responseThree === 'Y' || responseThree === 'YES') {
   correctAnswers++;
 } else {
   alert('I guess you don\'t know me very well...');
-  incorrectAnswers++;
 }
 console.log('responseThree', responseThree);
 
 console.log(correctAnswers);
-console.log(incorrectAnswers);
 
+//fourth question
 console.log('responseFour should be undefined:', responseFour);
 var responseFour = prompt('Do I have any pets?').toUpperCase();
 if (responseFour === 'Y' || responseFour === 'YES') {
@@ -58,13 +55,12 @@ if (responseFour === 'Y' || responseFour === 'YES') {
   correctAnswers++;
 } else {
   alert('I guess you don\'t know me very well...');
-  incorrectAnswers++;
 }
 console.log('responseFour', responseFour);
 
 console.log(correctAnswers);
-console.log(incorrectAnswers);
 
+// fifth question
 console.log('responseFive should be undefined:', responseFive);
 var responseFive = prompt('On that note, am I a dog person?').toUpperCase();
 if (responseFive === 'Y' || responseFive === 'YES') {
@@ -72,13 +68,12 @@ if (responseFive === 'Y' || responseFive === 'YES') {
   correctAnswers++;
 } else {
   alert('I guess you don\'t know me very well...');
-  incorrectAnswers++;
 }
 console.log('responseFive', responseFive);
 
 console.log(correctAnswers);
-console.log(incorrectAnswers);
 
+//sixth question
 var numbGuess = parseInt(prompt('For the next question, please respond with a number, rather than Y/N. Can you guess my favorite number?'));
 var guesses = 3;
 var flag = false;
@@ -93,28 +88,29 @@ while (guesses > 0 && flag === false) {
       break;
     } else if (numbGuess > 17) {
       numbGuess = prompt('Your guess was too high. Try again.');
-      incorrectAnswers++;
+  
     } else {
       numbGuess = prompt('Your guess was too low. Try again.');
-      incorrectAnswers++;
+  
     }
   }
 }
 
 console.log(correctAnswers);
-console.log(incorrectAnswers);
 
+//seventh question
 var possibleAnswers = ['CALIFORNIA', 'MONTANA', 'WASHINGTON', 'NORTH CAROLINA'];
 var question = prompt('For this final question, can you guess any of the 4 states I\'ve lived in?').toUpperCase();
-var guesses = 5;
-var flag = false;
-while (guesses > 0 && flag === false) {
-  guesses--;
+var guesses2 = 5;
+var flag2 = false;
+while (guesses2 > 0 && flag2 === false) {
+  guesses2--;
   for (var j = 0; j < possibleAnswers.length; j++); {
-    console.log('guesses', guesses);
+    console.log('guesses2', guesses2);
     if (question === possibleAnswers[j]) {
       alert('Great work!  It seems like you do know me pretty well.');
-      flag = true;
+      flag2 = true;
+      correctAnswers++;
       break;
     } else if (question !== correctAnswers[j]) {
       question = parseInt(prompt('Nope! I have lived a lot of places, but never there!'));
@@ -123,7 +119,6 @@ while (guesses > 0 && flag === false) {
 }
 
 console.log(correctAnswers);
-console.log(incorrectAnswers);
 
 if (correctAnswers >= 4) {
   alert('You got ' + correctAnswers + ' of the questions right, I guess you know me pretty well!');
